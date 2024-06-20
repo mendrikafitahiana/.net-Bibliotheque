2ef03594549404506a9d9229f1e5b287a657e4ff
mendrikafitahiana <ramilisonfita24@gmail.com>
2024-06-21 01:13:00 +0300

    ajout CRUD stock

---
+++ Bibtheque/Controllers/StockController.cs
@@ -0,0 +1,164 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Bibtheque.Models;
+using Bibtheque.Models.Context;
+
+namespace Bibtheque.Controllers
+{
+    public class StockController : Controller
+    {
+        private readonly BibthequeContext _context;
+
+        public StockController(BibthequeContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Stock
+        public async Task<IActionResult> Index()
+        {
+            var bibthequeContext = _context.Stock.Include(s => s.Livre);
+            return View(await bibthequeContext.ToListAsync());
+        }
+
+        // GET: Stock/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var stock = await _context.Stock
+                .Include(s => s.Livre)
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (stock == null)
+            {
+                return NotFound();
+            }
+
+            return View(stock);
+        }
+
+        // GET: Stock/Create
+        public IActionResult Create()
+        {
+            ViewData["LivreId"] = new SelectList(_context.Livre, "id", "id");
+            return View();
+        }
+
+        // POST: Stock/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,quantite,LivreId")] Stock stock)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(stock);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["LivreId"] = new SelectList(_context.Livre, "id", "id", stock.LivreId);
+            return View(stock);
+        }
+
+        // GET: Stock/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var stock = await _context.Stock.FindAsync(id);
+            if (stock == null)
+            {
+                return NotFound();
+            }
+            ViewData["LivreId"] = new SelectList(_context.Livre, "id", "id", stock.LivreId);
+            return View(stock);
+        }
+
+        // POST: Stock/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,quantite,LivreId")] Stock stock)
+        {
+            if (id != stock.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(stock);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StockExists(stock.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["LivreId"] = new SelectList(_context.Livre, "id", "id", stock.LivreId);
+            return View(stock);
+        }
+
+        // GET: Stock/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var stock = await _context.Stock
+                .Include(s => s.Livre)
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (stock == null)
+            {
+                return NotFound();
+            }
+
+            return View(stock);
+        }
+
+        // POST: Stock/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var stock = await _context.Stock.FindAsync(id);
+            if (stock != null)
+            {
+                _context.Stock.Remove(stock);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StockExists(int id)
+        {
+            return _context.Stock.Any(e => e.id == id);
+        }
+    }
+}
