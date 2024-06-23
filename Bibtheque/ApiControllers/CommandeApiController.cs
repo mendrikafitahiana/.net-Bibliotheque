c7979642db3f584ee64d7e226eefbfaa69355a3f
mendrikafitahiana <ramilisonfita24@gmail.com>
2024-06-23 08:39:10 +0300

    API get commande valide

--- Bibtheque/ApiControllers/CommandeApiController.cs
+++ Bibtheque/ApiControllers/CommandeApiController.cs
@@ -348,5 +348,162 @@ namespace Bibtheque.ApiControllers
 
             return Ok(result);
         }
+
+        [HttpGet("valide")]
+        public IActionResult GetCommandesValide(
+            string? recherche = null,
+            int pageNumber = 1,
+            int pageSize = 10
+        )
+        {
+
+            int userId = Convert.ToInt32(User.FindFirstValue(ClaimTypes.PrimarySid));
+
+            if (string.IsNullOrEmpty(userId.ToString()))
+            {
+                return Unauthorized("Utilisateur non authentifié.");
+            }
+
+            List<Commande> commandes = new List<Commande>();
+            int totalCount = 0;
+
+            string connectionString = _configuration.GetConnectionString("BibthequeContext");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                StringBuilder countSqlBuilder = new StringBuilder(@"
+                    SELECT COUNT(*) 
+                    FROM Commande c
+                    JOIN Livre l ON c.LivreId = l.id
+                    JOIN Utilisateur u ON c.UtilisateurId = u.id
+                    WHERE c.etat = 1 and c.UtilisateurId = @utilisateurId");
+
+                if (!string.IsNullOrEmpty(recherche))
+                {
+                    countSqlBuilder.Append(@"
+                    AND (
+                        l.titre LIKE @Recherche OR 
+                        l.auteur LIKE @Recherche OR 
+                        u.nom LIKE @Recherche OR 
+                        u.prenom LIKE @Recherche OR 
+                        CAST(c.dateCommande AS NVARCHAR) LIKE @Recherche OR
+                        CAST(l.prix AS NVARCHAR) LIKE @Recherche OR 
+                        CAST(l.nbPage AS NVARCHAR) LIKE @Recherche
+                    )");
+                }
+
+                using (SqlCommand countCmd = new SqlCommand(countSqlBuilder.ToString(), connection))
+                {
+                    countCmd.Parameters.AddWithValue("@utilisateurId", userId);
+                    if (!string.IsNullOrEmpty(recherche))
+                    {
+                        countCmd.Parameters.AddWithValue("@Recherche", $"%{recherche}%");
+                    }
+                    totalCount = (int)countCmd.ExecuteScalar();
+                }
+
+                // Requête principale avec pagination
+                StringBuilder sqlBuilder = new StringBuilder(@"
+                    SELECT * FROM 
+                    (
+                        SELECT ROW_NUMBER() OVER(ORDER BY c.id) AS RowNum, 
+                               c.id, c.quantite, c.total, c.etat, c.dateCommande,
+                               l.id AS LivreId, l.titre, l.auteur, l.resume, l.image, l.nbPage, l.prix, l.dateEdition,
+                                cat.id CategorieId, cat.nom categorieNom,
+                               u.id AS UtilisateurId, u.nom, u.prenom, u.numero, u.adresse
+                        FROM Commande c
+                        JOIN Livre l ON c.LivreId = l.id
+                        JOIN Categorie cat on l.CategorieId = cat.id
+                        JOIN Utilisateur u ON c.UtilisateurId = u.id
+                        WHERE c.etat = 1 and c.UtilisateurId = @utilisateurId");
+
+                if (!string.IsNullOrEmpty(recherche))
+                {
+                    sqlBuilder.Append(@"
+                        AND (
+                            l.titre LIKE @Recherche OR 
+                            l.auteur LIKE @Recherche OR 
+                            u.nom LIKE @Recherche OR 
+                            u.prenom LIKE @Recherche OR 
+                            CAST(c.dateCommande AS NVARCHAR) LIKE @Recherche OR
+                            CAST(l.prix AS NVARCHAR) LIKE @Recherche OR 
+                            CAST(l.nbPage AS NVARCHAR) LIKE @Recherche
+                        )");
+                }
+
+                sqlBuilder.Append(@") AS RowConstrainedResult
+                    WHERE RowNum >= @RowStart AND RowNum < @RowEnd
+                    ORDER BY RowNum");
+
+                using (SqlCommand command = new SqlCommand(sqlBuilder.ToString(), connection))
+                {
+                    int rowStart = (pageNumber - 1) * pageSize + 1;
+                    int rowEnd = pageNumber * pageSize + 1;
+                    command.Parameters.AddWithValue("@RowStart", rowStart);
+                    command.Parameters.AddWithValue("@RowEnd", rowEnd);
+                    command.Parameters.AddWithValue("@utilisateurId", userId);
+                    if (!string.IsNullOrEmpty(recherche))
+                    {
+                        command.Parameters.AddWithValue("@Recherche", $"%{recherche}%");
+                    }
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Commande commande = new Commande
+                            {
+                                id = reader.GetInt32(reader.GetOrdinal("id")),
+                                quantite = reader.GetInt32(reader.GetOrdinal("quantite")),
+                                total = reader.GetDouble(reader.GetOrdinal("total")),
+                                etat = reader.GetInt32(reader.GetOrdinal("etat")),
+                                dateCommande = DateOnly.FromDateTime(reader.GetDateTime(reader.GetOrdinal("dateCommande"))),
+                                LivreId = reader.GetInt32(reader.GetOrdinal("LivreId")),
+                                UtilisateurId = reader.GetInt32(reader.GetOrdinal("UtilisateurId")),
+                                Livre = new Livre
+                                {
+                                    id = reader.GetInt32(reader.GetOrdinal("LivreId")),
+                                    titre = reader.GetString(reader.GetOrdinal("titre")),
+                                    auteur = reader.GetString(reader.GetOrdinal("auteur")),
+                                    resume = reader.GetString(reader.GetOrdinal("resume")),
+                                    image = reader.GetString(reader.GetOrdinal("image")),
+                                    nbPage = reader.GetInt32(reader.GetOrdinal("nbPage")),
+                                    prix = reader.GetDouble(reader.GetOrdinal("prix")),
+                                    dateEdition = reader.GetDateTime(reader.GetOrdinal("dateEdition")),
+                                    CategorieId = reader.GetInt32(reader.GetOrdinal("CategorieId")),
+                                    Categorie = new Categorie
+                                    {
+                                        id = reader.GetInt32(reader.GetOrdinal("CategorieId")),
+                                        nom = reader.GetString(reader.GetOrdinal("CategorieNom"))
+                                    }
+                                },
+                                utilisateur = new Utilisateur
+                                {
+                                    id = reader.GetInt32(reader.GetOrdinal("UtilisateurId")),
+                                    nom = reader.GetString(reader.GetOrdinal("nom")),
+                                    prenom = reader.GetString(reader.GetOrdinal("prenom")),
+                                    adresse = reader.GetString(reader.GetOrdinal("adresse")),
+                                    numero = reader.GetString(reader.GetOrdinal("numero")),
+                                }
+                            };
+
+                            commandes.Add(commande);
+                        }
+                    }
+                }
+            }
+
+            var result = new
+            {
+                TotalCount = totalCount,
+                PageCount = (int)Math.Ceiling((double)totalCount / pageSize),
+                CurrentPage = pageNumber,
+                PageSize = pageSize,
+                Commandes = commandes
+            };
+
+            return Ok(result);
+        }
     }
 }
